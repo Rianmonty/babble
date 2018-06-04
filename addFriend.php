786d73079d096c0144091c5613f128016be7b6dd
Rian Montgomery <Rianmonty@californiacolleges.edu>
2018-06-05 00:52:01 +0100

    Update addFriend.php

--- addFriend.php
+++ addFriend.php
@@ -54,10 +54,10 @@
 session_start();
 $uid = $_SESSION['userID'];
 //Connection code to DB
-$dbUsername = "rian@babblechat-mysqldbserver";
+$dbUsername = "rian";
 $dbPassword = "BabbleApp1";
 $dbName = "babbledb";
-$serverName = "babblechat-mysqldbserver.mysql.database.azure.com";
+$serverName = "babblechat";
 $connection = mysqli_connect($serverName,$dbUsername,$dbPassword,$dbName);
 ?>
 </head>
